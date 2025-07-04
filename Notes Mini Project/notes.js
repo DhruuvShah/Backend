92e07db7872bc3ddb6026492b4f802f9c9634d9d
Dhruv271006011006 <dhruv27shah@gmail.com>
2025-07-04 18:48:58 +0530

    Notes Mini Project Completed with Create, Show, Update and Delete API

--- Notes Mini Project/notes.js
+++ Notes Mini Project/notes.js
@@ -6,15 +6,43 @@ app.use(express.json())
 
 let notes = []
 
-app.post('/notes',(req,res)=>{
+app.get('/', (req, res) => {
+    res.send('Hello, Dhruv!')
+})
+
+//Create Notes
+app.post('/notes', (req, res) => {
     console.log(req.body)
     notes.push(req.body)
     res.json({
-        message: "Notes added successfully",
-        notes: notes
+        message: "Note Created Successfully",
     })
 })
 
-app.listen(3000,()=>{
+//Shoow All Notes
+app.get('/notes', (req, res) => {
+    res.json(notes)
+})
+
+//Delete Notes
+app.delete('/notes/:index',(req,res)=>{
+    const index = req.params.index
+    delete notes[index]
+    res.json({
+        message: "Note Deleted Successfully",
+    })
+})
+
+//Update Notes
+app.patch('/notes/:index',(req,res)=>{
+    const index = req.params.index
+    const {title} = req.body
+    notes[index].title = title
+    res.json({
+        message: "Note Updated Successfully",
+    })
+})
+
+app.listen(3000, () => {
     console.log('Server is running on port 3000')
 })
