4bd95389fdad3b7c6f352471c39ce7cabe4d6d8f
Dhruv271006011006 <dhruv27shah@gmail.com>
2025-08-01 19:39:32 +0530

    Started New Project Social Media Caption Generator

--- Notes Mini Project Using Mongodb/src/db/db.js
+++ Notes Mini Project Using Mongodb/src/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 
 function connectToDB() {
-    mongoose.connect("Connection String")
+    mongoose.connect("mongodb+srv://dhruv27shah:gmd2qQieai1ss8aN@cluster0.se6xeas.mongodb.net/Cohort")
         .then(() => {
             console.log("Connected to DB")
         })
