fb1db91e2fca5f661d7b7e24baf2f1f6dc4eeaaa
Dhruv271006011006 <dhruv27shah@gmail.com>
2025-08-12 08:16:47 +0530

    Configure CORS to allow requests from specified frontend URLs

--- Moody Player/backend/src/app.js
+++ Moody Player/backend/src/app.js
@@ -4,7 +4,10 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+const frontendURL = "https://your-frontend-name.vercel.app";
+app.use(cors({
+  origin: [frontendURL, "http://localhost:5173"]
+}));
 
 app.use('/', songRoutes);
 
