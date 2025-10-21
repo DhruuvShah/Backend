a17133639e065e814f3e0944490e57af2a034701
Dhruv271006011006 <dhruv27shah@gmail.com>
2025-10-21 16:06:27 +0530

    Changed the folder names

--- Social Media Project/backend/src/app.js
+++ Social Media Project/backend/src/app.js
@@ -2,30 +2,24 @@ const express = require("express");
 const authRoutes = require("./routes/auth.routes");
 const postRoutes = require("./routes/post.routes");
 const cookieParser = require("cookie-parser");
-// const cors = require("cors");
-require("./models/user.model"); 
-require("./models/post.model"); 
+const cors = require("cors");
+require("./models/user.model");
+require("./models/post.model");
 
 const app = express();
 
-// app.use(
-//   cors({
-//     // origin: "http://localhost:5173", // Your frontend URL
-//     credentials: true,
-//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//   })
-// );
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
-// Middleware
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.json());
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../public/index.html"));
-// });
 
-// Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({
