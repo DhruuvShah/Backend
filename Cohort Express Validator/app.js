0874429cff54ce6f04ecb2b67bd882f87beafc2a
Dhruv271006011006 <dhruv27shah@gmail.com>
2025-12-25 15:34:16 +0530

    Add package.json with initial dependencies for Cohort Express Validator

---
+++ Cohort Express Validator/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const validator = require("./middleware/validator.middleware");
+
+const app = express();
+app.use(express.json());
+
+app.post("/register", validator.registerValidationRules, (req, res) => {
+  const { username, email, password } = req.body;
+    res.status(200).json({ message: "User registered successfully", data: { username, email } });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
